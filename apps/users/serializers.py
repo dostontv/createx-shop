28cc8065ce59d74f2e44198ce08bb2345b948ce9
behruzgithub <nigmatullayevbehruz5@gmail.com>
2024-11-03 14:18:59 +0500

    add validation to check password

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from . import models
 
@@ -49,6 +51,30 @@ class UserCreateSerializer(serializers.ModelSerializer):
             "password2",
         ]
 
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match.")
+
+        try:
+            validate_password(data['password1'])
+        except ValidationError as e:
+            raise serializers.ValidationError({"password1": list(e.messages)})
+
+        return data
+
+    def create(self, validated_data):
+        password = validated_data.pop('password1')
+        validated_data.pop('password2')
+
+        user = models.User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
@@ -62,3 +88,5 @@ class ReviewSerializer(serializers.ModelSerializer):
             "rating",
             "user_id",
         ]
+
+
